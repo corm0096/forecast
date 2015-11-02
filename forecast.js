e5f9492bb9f2d8ac4ed1a65a8200fe253dedc670
corm0096 <corm0096@algonquinlive.com>
2015-11-01 20:40:49 -0500

    Working version.

--- forecast.js
+++ forecast.js
@@ -1,19 +1,10 @@
-
-//Ajax query for weather.
-//Code to call up the widget-proper and the CSS.
-	//https://api.forecast.io/forecast/c37544ed79ac04ac45e78eb9cc1ae798/45.411, -75.706
-//The above contains a key and incorrect lat/long.
-
-
-
 function forecast()
 {
 	$.ajax(
 		{
-			url:"https://api.forecast.io/forecast/c37544ed79ac04ac45e78eb9cc1ae798/45.411,-75.706",
-			dataType:"json",
-			method:"GET",
-			units:"ca"
+			url:"https://api.forecast.io/forecast/c37544ed79ac04ac45e78eb9cc1ae798/45.411,-75.706?units=ca",
+			dataType:"jsonp",
+			method:"GET"
 		})
 		.done(forecastloaded)
 		.fail();
@@ -21,5 +12,28 @@ function forecast()
 
 function forecastloaded(data,textStatus,jOBJ)
 {
-	console.log(data);
+	$("#forecast").append("<table></table>");
+	$("#forecast table").append("<thead></thead>");
+	$("#forecast thead").append("<tr></tr>");
+	$("#forecast tr").append(
+		"<th>% humidity</th>",
+		"<th>% cloud cover</th>",
+		"<th>Temp (C)</th>",
+		"<th>wind speed (km/hr)</th>",
+		"<th></th>","<th>Summary</th>");
+	$("#forecast table").append("<tbody></tbody>");
+		
+	$.each(data.hourly.data, writeline);
+}
+
+function writeline(index,weatherdata)
+{	
+	$("#forecast tbody").append("<tr></tr>", [
+			"<td>"+Math.round(weatherdata.humidity*100)+"</td>",
+			"<td>"+Math.round(weatherdata.cloudCover*100)+"</td>",
+			"<td>"+weatherdata.temperature+"</td>",
+			"<td>"+weatherdata.windSpeed+"</td>",
+			"<td>"+weatherdata.icon+"</td>",
+			"<td>"+weatherdata.summary+"</td>"
+	]);	
 }
